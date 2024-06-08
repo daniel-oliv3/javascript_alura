89ae26a085456e59bad38d0f9d76d629a48b7c3f
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2024-06-08 11:40:14 -0300

    O que aprendemos?

---
+++ javascript_07/javascript_15/Conta.js
@@ -0,0 +1,59 @@
+export class Conta {
+    constructor (saldoInicial, cliente, agencia){
+        this._saldo = saldoInicial; //_privado
+        this._cliente = cliente;
+        this._agencia = agencia;
+    }
+
+    // Get e Set
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
+
+    //Metodos
+    sacar(valor){
+        let taxa = 1;
+        return this._sacar(valor, taxa);
+        // const valorSacado = taxa * valor;
+        // if(this._saldo >= valorSacado) {
+        //     this._saldo -= valorSacado;
+        //     console.log('Saque efetuado com sucesso!');
+        //     return valorSacado;
+        // }
+    }
+
+    _sacar(valor, taxa){
+        const valorSacado = taxa * valor;
+        if(this._saldo >= valorSacado) {
+            this._saldo -= valorSacado;
+            return valorSacado;
+        }
+
+        return 0;
+    }
+
+    depositar(valor){ 
+        this._saldo += valor;
+        console.log('Depósito efetuado com sucesso!');
+    }
+
+    transferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+        console.log('Transferência efetuada com sucesso!');
+    }
+
+    teste(){
+        console.log("Teste na classe conta");
+    }
+}
